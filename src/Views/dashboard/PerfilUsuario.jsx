999e54423f0770d727f3182de7860ab2b119469d
ronaldo-martinez <irmv_0311@hotmial.com>
2022-10-11 03:40:37 -0600

    Configuracion de perfil de usuario terminada

--- src/Views/dashboard/PerfilUsuario.jsx
+++ src/Views/dashboard/PerfilUsuario.jsx
@@ -1,30 +1,75 @@
 
-import PhotoCameraIcon from '@mui/icons-material/PhotoCamera';
 import img from '../../img/profile.png';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import AuthUser from "../../components/AuthUser";
 
-import { Button } from '@mui/material';
+import { 
+    Button,
+    Alert
+} from '@mui/material';
 
 import {
     MDBInputGroup
   } from 'mdb-react-ui-kit';
 
 
+const fileInitial={
+    archivo:null,
+    archivoNombre:""
+}
 
 function PerfilUsuario({baseURL}) {
     const [nombreUser, setNombreUser]=useState("");
     const [fechaUser, setFechaUser]=useState("");
     const [emailUser, setEmail]=useState("");
-    const [imagenUser, setImagenUser]=useState("")
+    const [imagenUsuario, setImagenUsuario]=useState(fileInitial);
+    const [imagenUrlUser, setImagenUrlUser]=useState("")
+    const [successEdit, setSuccessEdit]=useState(false);
+    const [invalidData, setInvalidData]=useState(false);
+    const [invalidMessage, setInvalidMessage]=useState(false);
     const {http}=AuthUser();
+    const fileRef = useRef(null)
+
+    //Se ejecuta cuando se selecciona un archivo (imagen)
+    const fileSelect=(e)=>{
+        setImagenUsuario({
+            archivo: e.target.files[0],
+            archivoNombre:e.target.files[0].name,
+        });
+
+    }
 
     const consultarUsuario=()=>{
         http.post('/me').then((data)=>{
             setNombreUser(data.data.name);
             setFechaUser(data.data.fecha_nacimiento);
             setEmail(data.data.email);
-            setImagenUser(data.data.imagen)
+            setImagenUrlUser(data.data.imagen)
+        });
+    }
+
+    const editarUsuario=()=>{    
+        setSuccessEdit(false);
+        const formData=new FormData();
+        formData.append('name', nombreUser)
+        formData.append('email', emailUser)
+        formData.append('fecha_nacimiento', fechaUser)
+        imagenUsuario.archivo?
+        formData.append('imagen', imagenUsuario.archivo, imagenUsuario.archivoNombre)
+        :
+        formData.append('imagen','')
+        http.post("me/edit", formData).then((data)=>{
+            //Si hay un dato invalido
+            if(data.data.type){
+                setInvalidData(true);
+                setInvalidMessage(data.data.message)
+            }
+            if(data.data==="success"){
+                setImagenUsuario(fileInitial);
+                fileRef.current.value="";
+                setSuccessEdit(true);
+                consultarUsuario();
+            }
         });
     }
 
@@ -104,16 +149,31 @@ function PerfilUsuario({baseURL}) {
                                             <Button
                                                 variant="outlined" 
                                                 color="primary"
+                                                onClick={editarUsuario}
                                             >
                                                 Actualizar Perfil
                                             </Button>
-                                        </div>  
+                                        </div> 
+                                        {
+                                            successEdit?
+                                                <Alert severity="success">Perfil Modificado</Alert>
+                                                :
+                                                ""
+                                        }
+                                        {
+                                            invalidData? 
+                                                <div className='pb-4'>
+                                                    <Alert severity="error">{invalidMessage}</Alert>
+                                                </div>
+                                                :
+                                                ""
+                                        } 
                                         
                                     </div> 
                                     <div className="col-4 bg d-none d-md-block" >
                                         <div className='img-holder'align="center">
-                                            {imagenUser?
-                                                <img src={`${baseURL}${imagenUser}`} width="150" height="150"alt='Imagen de Perfil'/>
+                                            {imagenUrlUser?
+                                                <img src={`${baseURL}${imagenUrlUser}`} width="150" height="150"alt='Imagen de Perfil'/>
                                                 :
                                                 <img src={img} width="150" height="150"alt='Imagen de Perfil'/>
                                             }
@@ -121,7 +181,17 @@ function PerfilUsuario({baseURL}) {
                                         </div>
                                     
                                         <div className='w-100  py-4 ' align="center" >
-                                        <button type="button" className="btn btn-outline-primary">Modificar Foto   <PhotoCameraIcon></PhotoCameraIcon></button>
+                                        {/* Input para subir imagen */}
+                                        <div className="m-3">
+                                            <label htmlFor="formFileSm" className="form-label">Modificar Foto.</label>
+                                            <input 
+                                                ref={fileRef}
+                                                className="form-control form-control-sm" 
+                                                id="formFileSm" 
+                                                type="file"
+                                                onChange={fileSelect}
+                                            />
+                                        </div>
 
                                         </div>
                                     </div>  
